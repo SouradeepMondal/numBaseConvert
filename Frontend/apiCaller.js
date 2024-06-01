8ea9739ff1197c860bfdc92972cd5ba96f447d6a
SouradeepMondal <78356708+SouradeepMondal@users.noreply.github.com>
2024-06-01 14:52:58 +0530

    api working

--- Frontend/apiCaller.js
+++ Frontend/apiCaller.js
@@ -1,18 +1,11 @@
 // Example: GET request
 async function userAction (number, inputBase, outputBase) {
-        const response = await fetch('http://localhost:5000/api/v1.0/numBaseConvert?number=10&inputBase=10&outputBase=10')
-        .then(response => {
-            if (!response.ok) {
-              throw new Error('Network response was not ok');
-            }
-            return response.json();
-          })
-        //   .then(data => {
-        //     console.log('Data received:', data);
-        //   })
-          .catch(error => {
-            console.error('There was a problem with the fetch operation:', error);
-          });
+  const response = await fetch('http://localhost:5000/api/v1.0/numBaseConvert?number=10&inputBase=10&outputBase=10')
+  const data = await response.json()
+  var output = document.getElementById('outputNumber');
+  output.innerHTML = data['Output Number']
+  console.log(data)
+        
 };
 
 // // Example: POST request
